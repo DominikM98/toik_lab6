a7266ee145142a4cb19b8578ac2164f4043832d4
Dominik Majcher <majcher.dominik@op.pl>
2020-11-10 12:17:52 +0100

    Inital implementation of the problem

---
+++ src/com/company/QuizImpl.java
@@ -0,0 +1,14 @@
+package com.company;
+
+class QuizImpl implements Quiz{
+    @Override
+    public void isCorrectValue(int value) throws ParamTooLarge, ParamTooSmall {
+
+    }
+
+    private int digit;
+
+    public QuizImpl(){
+        this.digit = 254;
+    }
+}
